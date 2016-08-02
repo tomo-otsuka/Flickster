973097c9f736c39db9e74f7916d57e16485b3c68
Tomo Otsuka <totsuka@hearsaycorp.com>
2016-08-01 21:42:03 -0700

    Basic requirements done

---
+++ app/src/main/java/com/example/totsuka/flickster/adapters/MovieArrayAdapter.java
@@ -0,0 +1,49 @@
+package com.example.totsuka.flickster.adapters;
+
+import com.example.totsuka.flickster.R;
+import com.example.totsuka.flickster.models.Movie;
+import com.squareup.picasso.Picasso;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.List;
+
+/**
+ * Created by totsuka on 8/1/16.
+ */
+public class MovieArrayAdapter extends ArrayAdapter<Movie> {
+
+    public MovieArrayAdapter(Context context, List<Movie> movies) {
+        super(context, android.R.layout.simple_list_item_1, movies);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        Movie movie = getItem(position);
+
+        if (convertView == null) {
+            LayoutInflater inflater = LayoutInflater.from(getContext());
+            convertView = inflater.inflate(R.layout.item_movie, parent, false);
+        }
+
+        ImageView ivImage = (ImageView) convertView.findViewById(R.id.ivMovieImage);
+
+        ivImage.setImageResource(0);
+
+        TextView tvTitle = (TextView) convertView.findViewById(R.id.tvTitle);
+        TextView tvOverview = (TextView) convertView.findViewById(R.id.tvOverview);
+
+        tvTitle.setText(movie.getOriginalTitle());
+        tvOverview.setText(movie.getOverview());
+
+        Picasso.with(getContext()).load(movie.getPosterPath()).into(ivImage);
+
+        return convertView;
+    }
+}
